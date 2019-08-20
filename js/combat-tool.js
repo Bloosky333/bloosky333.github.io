e3e4509171c4f7813239f7ae5f45d4b2f041f465
Tianwei Wang <tianwei.wng@gmail.com>
2019-08-21 01:30:36 +0200

    WIP

--- js/combat-tool.js
+++ js/combat-tool.js
@@ -1,9 +1,14 @@
 Vue.component('stat-line', {
-    props: ['stat', 'is_buff'],
+    props: ['stat'],
     methods: {
         decreaseStat: function () {
             if(this.stat.current > 0) {
                 this.stat.current--;
+            }
+        },
+        increaseStat: function () {
+            if(this.stat.current < this.stat.max) {
+                this.stat.current++;
             }
         },
         resetStat: function () {
@@ -16,7 +21,7 @@ Vue.component('stat-line', {
     },
     template: `
     <div class="row">
-        <div class="col-sm-8 mb-2">
+        <div class="col-sm-7 mb-2">
             <div class="input-group">
                 <div class="input-group-prepend">
                     <span class="input-group-text text-capitalize input-group-text-fixed-w">{{ stat.title }}</span>
@@ -25,8 +30,9 @@ Vue.component('stat-line', {
                 <input type="text" class="form-control text-center" v-model="stat.max">
             </div>
         </div>
-        <div class="col-sm-4 mb-2 text-right">
+        <div class="col-sm-5 mb-2 text-right">
             <div class="btn-group"
+                <button class="btn btn-success" type="button" @click="increaseStat()"><i class="fa fa-plus"></i></button>
                 <button class="btn btn-warning" type="button" @click="decreaseStat()"><i class="fa fa-minus"></i></button>
                 <button class="btn btn-secondary" type="button" @click="resetStat()"><i class="fa fa-refresh"></i></button>
                 <button class="btn btn-secondary" type="button" @click="emptyStat()"><i class="fa fa-trash"></i></button>
@@ -34,7 +40,75 @@ Vue.component('stat-line', {
         </div>
     </div>
   `
-})
+});
+Vue.component('hit-rate-table', {
+    props: ['bab', 'crit'],
+    data: function() {
+        let data = {
+            minAC: 20,
+            maxAC: 40,
+        };
+
+        data.headers = ["BAB/AC"];
+        for (let ac = data.minAC; ac <= data.maxAC; ac++) {
+            data.headers.push(ac);
+        }
+
+        return data;
+    },
+    computed: {
+        table: function() {
+
+            let data = [], row, header;
+            let minOffset = -5;
+            let maxOffset = 6;
+            let bab = parseInt(this.bab, 10);
+            for (let offset = minOffset; offset <= maxOffset; offset++) {
+                row = [];
+                header = offset < 0 ? offset : "+" + offset;
+
+                for (let ac = this.minAC; ac <= this.maxAC; ac++) {
+                    row.push(this._getHitRate(bab + offset, ac, this.crit));
+                }
+                data.push({
+                    header: (bab + offset) + " (" + header + ")",
+                    row: row
+                });
+            }
+            return data;
+        },
+    },
+    methods: {
+        _getHitRate: function (bab, ac, critDice) {
+            let critMiss = 1/20;
+            let crit = (21 - critDice) / 20;
+            let hit = (21 + bab - ac) / 20;
+            if(hit>=1){
+                hit = 1 - critMiss;
+            } else if(hit<=crit) {
+                hit = crit;
+            }
+            return Math.round(hit * 100);
+        }
+    },
+    template: `
+    <div class="table-responsive">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th v-for="header in headers">{{header}}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in table">
+                    <th>{{row.header}}</th>
+                    <td v-for="cell in row.row" :class="{'table-success': cell>=80, 'table-warning': cell<80 && cell >=50, 'table-danger': cell<50}">{{ cell }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  `
+});
 
 var combatTool = new Vue({
     el: '#combat-tool',
@@ -45,6 +119,8 @@ var combatTool = new Vue({
             level: 10,
             damage_mod: 0,
             current_turn: 0,
+            bab: 17,
+            crit: 20,
         };
         let stats = {
             hp: 100,
